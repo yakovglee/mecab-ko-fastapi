9dffa13a91ebd92e819ab5c616441fd45336da2e
yakovglee <yakovglee@gmail.com>
2025-10-08 12:52:28 +0900

    add parsing n refactor

--- utils.py
+++ utils.py
@@ -1,8 +1,9 @@
+import re
 import mecab_ko as MeCab
-from wordtypes import MecabKoDicWord
+from wordtypes import Data, MecabKoDicWord, ParsedExpression
 
 
-def parse_text(text: str) -> list[MecabKoDicWord]:
+def parse_text(text: str) -> list[Data]:
     """Парсит текст с помощью MeCab и возвращает список токенов."""
 
     tagger = MeCab.Tagger()
@@ -16,18 +17,36 @@ def parse_text(text: str) -> list[MecabKoDicWord]:
         surface, feature_str = line.split("\t")
         features = feature_str.split(",")
 
-        token = MecabKoDicWord(
-            surface=surface,
-            pos=features[0],
-            semantic_class=features[1],
-            has_final_consonant=(features[2].upper() == "T"),
-            reading=features[3],
-            entry_type=features[4],
-            first_pos=features[5],
-            last_pos=features[6],
-            expression=features[7],
-        )
+        mecab_word = make_mecab(surface, features)
 
-        tokens.append(token)
+        if mecab_word.expression != "*":
+            parsed_exp = parse_expression(mecab_word.expression)
+            tokens.append(Data(word=mecab_word, parsed_expression=parsed_exp))
+        else:
+            tokens.append(Data(word=mecab_word))
 
     return tokens
+
+
+def make_mecab(surface: str, features: list[str]) -> MecabKoDicWord:
+    return MecabKoDicWord(
+        surface=surface,
+        pos=features[0],
+        semantic_class=features[1],
+        has_final_consonant=(features[2].upper() == "T"),
+        reading=features[3],
+        entry_type=features[4],
+        first_pos=features[5],
+        last_pos=features[6],
+        expression=features[7],
+    )
+
+
+def parse_expression(s: str) -> list[ParsedExpression]:
+    pattern = re.compile(r"([^+/]+)/([A-Z]+?)/\*")
+
+    res = []
+    for lemma, pos in pattern.findall(s):
+        res.append(ParsedExpression(lemma=lemma, pos=pos))
+
+    return res
