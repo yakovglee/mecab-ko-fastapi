42ff7878162cc7a355a15a9412b53782a34abddc
yakovglee <yakovglee@gmail.com>
2025-10-08 12:54:34 +0900

    add status code

--- app.py
+++ app.py
@@ -1,9 +1,19 @@
-from fastapi import FastAPI, Body
+from fastapi import FastAPI, Body, HTTPException
 from utils import parse_text
+from wordtypes import Data
 
 app = FastAPI()
 
 
-@app.post("/")
+@app.post("/", response_model=list[Data])
 def read_root(text: str = Body(..., embed=True)):
-    return parse_text(text)
+
+    try:
+        tokens = parse_text(text)
+        if not tokens:
+            raise HTTPException(status_code=404, detail="Не удалось распарсить текст")
+        return tokens
+    except ValueError as e:
+        raise HTTPException(status_code=400, detail=f"Ошибка в данных: {e}")
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Ошибка парсинга: {e}")
